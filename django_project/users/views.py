4733ac4ac41848519402fbdf6f442dc9ec2f8e93
onlyabhilash <77659887+onlyabhilash@users.noreply.github.com>
2021-04-11 13:11:38 +0530

    Hi

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -5,7 +5,9 @@ from django.contrib import messages
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Accounted created for {username}!')
             return redirect('blog-home')
